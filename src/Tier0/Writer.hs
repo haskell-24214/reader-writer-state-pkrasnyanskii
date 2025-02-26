709af1a35e736246252e6fa23ffcf5df6f5a207b
pkrasnyanskii <p.krasnyanskii@g.nsu.ru>
2025-02-27 02:13:24 +0700

    HW Done (Notice: Changed module import in test2 to prevent issues)

--- src/Tier0/Writer.hs
+++ src/Tier0/Writer.hs
@@ -1,8 +1,15 @@
-module Tier0.Writer (Tree (..), sumAndTraceInOrder) where
+module Tier0.Writer where
 
 import Control.Monad.Writer
 
 data Tree a = Leaf a | Branch (Tree a) a (Tree a) deriving Eq
 
 sumAndTraceInOrder :: Num a => Tree a -> Writer [a] a
-sumAndTraceInOrder = undefined
+sumAndTraceInOrder (Leaf x) = do
+  tell [x]
+  return x
+sumAndTraceInOrder (Branch left x right) = do
+  leftSum <- sumAndTraceInOrder left
+  tell [x]
+  rightSum <- sumAndTraceInOrder right
+  return (leftSum + x + rightSum)
